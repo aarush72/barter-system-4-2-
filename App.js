3118a8e62aaf708ff566369004c01f78d08c1648
aarush72 <72920939+aarush72@users.noreply.github.com>
2021-06-10 11:46:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+import {AppTabNavigator} from './components/AppTabNavigator'
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation'; 
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+
+export default function App() { 
+  return ( 
+  <AppContainer/> 
+  ); 
+} 
+  
+const switchNavigator = 
+createSwitchNavigator({ 
+  WelcomeScreen:{screen: WelcomeScreen}, 
+  BottomTab : {screen: AppTabNavigator}
+}) 
+
+const AppContainer = createAppContainer(switchNavigator);
+
